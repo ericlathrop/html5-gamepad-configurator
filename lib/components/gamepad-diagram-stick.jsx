c409d103dcc57f9de8cac11396d9605a16060f2a
Eric Lathrop <eric@ericlathrop.com>
2016-11-13 15:03:28 -0500

    Add axis buttons

--- lib/components/gamepad-diagram-stick.jsx
+++ lib/components/gamepad-diagram-stick.jsx
@@ -1,14 +1,21 @@
 import pure from "./pure";
 import React from "react";
 
-export default pure(function GamepadDiagramAxis({ name, onClick, xValue, yValue }) {
+export default pure(function GamepadDiagramStick({ name, onClick, xValue, yValue }) {
   var className = name.replace(/ /g, "-");
   return (
-    <button
-      className={className + " gamepad-diagram-stick"}
-      name={name}
-      onClick={e => onClick(e.target.name)}
-      title={name}>
+    <div className={className + " gamepad-diagram-stick"}>
       <div className="gamepad-diagram-stick-pos" style={{ left: ((1.0 + xValue) / 2.0) * 100.0 + "%", top: ((1.0 + yValue) / 2.0) * 100.0 + "%" }} />
-    </button>);
+      <button
+        className="gamepad-diagram-stick-x"
+        name={name + " x"}
+        onClick={e => onClick(e.target.name)}
+        title={name}>X</button>
+      <button
+        className="gamepad-diagram-stick-y"
+        name={name + " y"}
+        onClick={e => onClick(e.target.name)}
+        title={name}>Y</button>
+    </div>
+  );
 });
